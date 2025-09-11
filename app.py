57372c4e24e734b854a18ef282d5fe75026ca2a4
baris <baris.ock.etudes@gmail.com>
2025-09-11 17:34:13 +0200

    ajout carousel

--- app.py
+++ app.py
@@ -8,13 +8,25 @@ DB_URL = "postgresql://postgres:root@127.0.0.1:5432/movies"
 
 def get_movies():
     with psycopg2.connect(DB_URL) as conn, conn.cursor() as cur:
-        cur.execute("SELECT title, poster_url, genres FROM movies ORDER BY release_date DESC;")
-        return cur.fetchall()    
+        cur.execute("SELECT title, poster_url, genres, backdrop_url FROM movies ORDER BY release_date DESC;")
+        rows = cur.fetchall()  
+
+        movies = [
+            {
+                "title": row[0],
+                "poster_url": row[1],
+                "genres": row[2],
+                "backdrop_url": row[3]
+            }
+            for row in rows
+        ]  
+        return movies
 
 @app.route("/")
 def index():
     movies = get_movies()
     return render_template("index.html", movies=movies)
+
 
 @app.route("/contact")
 def contact():
